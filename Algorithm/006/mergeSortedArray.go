f52fa12fb7c553ead4ae1e2963c1e36913ac5e03
ratiki <987090402@qq.com>
2019-06-14 17:24:20 +0900

    合并排序数组 II

--- Algorithm/006/mergeSortedArray.go
+++ Algorithm/006/mergeSortedArray.go
@@ -12,3 +12,34 @@ func mergeSortedArray(arr1, arr2 []int) []int {
 	sort.Ints(tmp)
 	return tmp
 }
+
+func mergeSortedArray2(arr1, arr2 []int) []int {
+	if arr1[0] >= arr2[len(arr2)-1] {
+		return append(arr2, arr1...)
+	} else if arr2[0] >= arr1[len(arr1)-1] {
+		return append(arr1, arr2...)
+	}
+	countArr1 := 0
+	countArr2 := 0
+	var tmp = make([]int, 0)
+	for countArr1 != len(arr1) && countArr2 != len(arr2) {
+		if arr1[countArr1] < arr2[countArr2] {
+			tmp = append(tmp, arr1[countArr1])
+			countArr1++
+		} else if arr1[countArr1] == arr2[countArr2] {
+			tmp = append(tmp, arr1[countArr1], arr2[countArr2])
+			countArr1++
+			countArr2++
+		} else {
+			tmp = append(tmp, arr2[countArr2])
+			countArr2++
+		}
+	}
+	if countArr1 == len(arr1) {
+		tmp = append(tmp, arr2[countArr2:]...)
+	}
+	if countArr2 == len(arr2) {
+		tmp = append(tmp, arr1[countArr1:]...)
+	}
+	return tmp
+}
--- Algorithm/006/mergeSortedArray_test.go
+++ Algorithm/006/mergeSortedArray_test.go
@@ -22,3 +22,21 @@ func TestMergeSortedArray(t *testing.T) {
 		}
 	}
 }
+
+func TestMergeSortedArray2(t *testing.T) {
+	tests := []struct {
+		arrA, arrB []int
+		wantArr    []int
+	}{
+		{[]int{1, 2}, []int{1, 2}, []int{1, 1, 2, 2}},
+		{[]int{1, 2, 3}, []int{4, 5, 6}, []int{1, 2, 3, 4, 5, 6}},
+		{[]int{1, 4}, []int{2, 6}, []int{1, 2, 4, 6}},
+	}
+
+	for _, test := range tests {
+		result := mergeSortedArray2(test.arrA, test.arrB)
+		if !reflect.DeepEqual(result, test.wantArr) {
+			t.Errorf("mergeSortedArray(%v,%v) => %v,want %v", test.arrA, test.arrB, result, test.wantArr)
+		}
+	}
+}
