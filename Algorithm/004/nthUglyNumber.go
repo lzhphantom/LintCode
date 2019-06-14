2325dd6ba9f905cc47c21541bb51510fd4af4fa0
ratiki <987090402@qq.com>
2019-06-14 12:49:42 +0900

    丑数

--- Algorithm/004/nthUglyNumber.go
+++ Algorithm/004/nthUglyNumber.go
@@ -1,5 +1,42 @@
 package _04
 
 func uthUglyNumber(n int) int {
+	var uglyNumberArray = map[int]int{
+		2: 0,
+		3: 0,
+		5: 0,
+	}
+	var numberArray = []int{1}
 
+	for len(numberArray) < n {
+		num2 := numberArray[uglyNumberArray[2]] * 2
+		num3 := numberArray[uglyNumberArray[3]] * 3
+		num5 := numberArray[uglyNumberArray[5]] * 5
+		minNum := min(num2, num3, num5)
+		if minNum == num2 {
+			uglyNumberArray[2] += 1
+		} else if minNum == num3 {
+			uglyNumberArray[3] += 1
+		} else if minNum == num5 {
+			uglyNumberArray[5] += 1
+		}
+		if minNum == numberArray[len(numberArray)-1] {
+			continue
+		} else {
+			numberArray = append(numberArray, minNum)
+		}
+	}
+
+	return numberArray[n-1]
+}
+
+func min(a, b, c int) int {
+	tmp := a
+	if b < tmp {
+		tmp = b
+	}
+	if c < tmp {
+		tmp = c
+	}
+	return tmp
 }
--- Algorithm/004/nthUglyNumber_test.go
+++ Algorithm/004/nthUglyNumber_test.go
@@ -1,1 +1,23 @@
 package _04
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNthUglyNumber(t *testing.T) {
+	tests := []struct {
+		n    int
+		want int
+	}{
+		{9, 10},
+		{10, 12},
+	}
+
+	for _, test := range tests {
+		result := uthUglyNumber(test.n)
+		if !reflect.DeepEqual(test.want, result) {
+			t.Errorf("uthUglyNumber(%d) => %d,want %d", test.n, result, test.want)
+		}
+	}
+}
