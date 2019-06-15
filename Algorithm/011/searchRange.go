50aac0907c425eeb92228aa95b319f4da8490a95
lzhphantom <lzhphantom2014@gmail.com>
2019-06-15 17:33:33 +0800

    二叉查找树中搜索区间 method2

--- Algorithm/011/searchRange.go
+++ Algorithm/011/searchRange.go
@@ -4,7 +4,7 @@ import "sort"
 
 type TreeNode struct {
 	Val         int
-	left, right *TreeNode
+	Left, Right *TreeNode
 }
 
 func searchRange(root *TreeNode, k1, k2 int) []int {
@@ -21,10 +21,39 @@ func compareSearchRange(root *TreeNode, list *[]int, k1, k2 int) {
 	if root.Val >= k1 && root.Val <= k2 {
 		*list = append(*list, root.Val)
 	}
-	if root.left != nil {
-		compareSearchRange(root.left, list, k1, k2)
+	if root.Left != nil {
+		compareSearchRange(root.Left, list, k1, k2)
 	}
-	if root.right != nil {
-		compareSearchRange(root.right, list, k1, k2)
+	if root.Right != nil {
+		compareSearchRange(root.Right, list, k1, k2)
 	}
+}
+
+/**
+ * @param root: param root: The root of the binary search tree
+ * @param k1: An integer
+ * @param k2: An integer
+ * @return: return: Return all keys that k1<=key<=k2 in ascending order
+ */
+func searchRange1(root *TreeNode, k1 int, k2 int) []int {
+	if root == nil {
+		return []int{}
+	}
+	if root.Left == nil && root.Right == nil {
+		if root.Val >= k1 && root.Val <= k2 {
+			return []int{root.Val}
+		}
+		return []int{}
+	}
+	result := make([]int, 0)
+	if root.Left != nil && root.Val > k1 {
+		result = append(result, searchRange(root.Left, k1, k2)...)
+	}
+	if root.Val >= k1 && root.Val <= k2 {
+		result = append(result, root.Val)
+	}
+	if root.Right != nil && root.Val < k2 {
+		result = append(result, searchRange(root.Right, k1, k2)...)
+	}
+	return result
 }
--- Algorithm/011/searchRange_test.go
+++ Algorithm/011/searchRange_test.go
@@ -60,3 +60,59 @@ func TestSearchRange(t *testing.T) {
 		}
 	}
 }
+
+func TestSearchRange1(t *testing.T) {
+	tests := []struct {
+		root   *TreeNode
+		k1, k2 int
+		want   []int
+	}{
+		{
+			&TreeNode{
+				3,
+				&TreeNode{
+					4,
+					nil, nil,
+				},
+				&TreeNode{
+					8,
+					nil, nil,
+				},
+			},
+			3, 10,
+			[]int{3, 4, 8},
+		},
+		{
+			&TreeNode{
+				1,
+				&TreeNode{
+					5,
+					&TreeNode{
+						Val: 9,
+					},
+					&TreeNode{
+						Val: 20,
+					},
+				},
+				&TreeNode{
+					13,
+					&TreeNode{
+						Val: 10,
+					},
+					&TreeNode{
+						Val: 31,
+					},
+				},
+			},
+			7, 14,
+			[]int{9, 10, 13},
+		},
+	}
+
+	for _, test := range tests {
+		list := searchRange1(test.root, test.k1, test.k2)
+		if !reflect.DeepEqual(list, test.want) {
+			t.Errorf("searchRange1(%v,%d,%d) => %v,want %v", test.root, test.k1, test.k2, list, test.want)
+		}
+	}
+}
