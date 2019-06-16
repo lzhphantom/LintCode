172fc78dacf794db9c67d13ce6cda8def50c0b6f
lzhphantom <lzhphantom2014@gmail.com>
2019-06-16 10:23:35 +0800

    二分查找

---
+++ Algorithm/014/binarySearch.go
@@ -0,0 +1,28 @@
+package _14
+
+func binarySearch(nums []int, target int) int {
+	start := 0
+	end := len(nums) - 1
+
+	index := -1
+	if nums[start] == target {
+		return start
+	}
+	if nums[end] == target {
+		return end
+	}
+
+	for end-start != 1 {
+		mid := (start + end) / 2
+		if nums[mid] > target {
+			end = mid
+		} else if nums[mid] < target {
+			start = mid
+		} else {
+			index = mid
+			end--
+		}
+	}
+
+	return index
+}
---
+++ Algorithm/014/binarySearch_test.go
@@ -0,0 +1,42 @@
+package _14
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		want   int
+	}{
+		{
+			[]int{1, 2, 3, 4, 5, 6, 7},
+			4,
+			3,
+		},
+		{
+			[]int{1, 2, 3, 3, 4, 5, 10},
+			3,
+			2,
+		},
+		{
+			[]int{1, 2, 3, 3, 4, 5, 10},
+			6,
+			-1,
+		},
+		{
+			[]int{1, 2, 3, 3, 4, 5, 10},
+			1,
+			0,
+		},
+	}
+
+	for _, test := range tests {
+		result := binarySearch(test.nums, test.target)
+		if !reflect.DeepEqual(result, test.want) {
+			t.Errorf("binarySearch(%v, %d) => %d,want %d", test.nums, test.target, result, test.want)
+		}
+	}
+}
