e3ee81d22bb3723d2d2e4a3ac007d6d5e06413a2
ratiki <987090402@qq.com>
2019-06-24 19:53:00 +0900

    N皇后问题 ready

---
+++ Algorithm/033/solveNQueens.go
@@ -0,0 +1,6 @@
+package _33
+
+func solveNQueens(n int) [][]string {
+	result := make([][]string, 0)
+	return result
+}
---
+++ Algorithm/033/solveNQueens_test.go
@@ -0,0 +1,37 @@
+package _33
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSolveNQueens(t *testing.T) {
+	tests := []struct {
+		n    int
+		want [][]string
+	}{
+		{
+			1,
+			[][]string{
+				{"Q"},
+			},
+		},
+		{
+			4,
+			[][]string{
+				{".Q..", "...Q", "Q...", "..Q."},
+				{"Q...", ".Q..", "..Q.", "...Q"},
+				{"..Q.", "...Q", ".Q..", "Q..."},
+				{"...Q", "Q...", ".Q..", "..Q."},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		result := solveNQueens(test.n)
+
+		if !reflect.DeepEqual(test.want, result) {
+			t.Errorf("solveNQueens(%d) => %v, want %v", test.n, result, test.want)
+		}
+	}
+}
