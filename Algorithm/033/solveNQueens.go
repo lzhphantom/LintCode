50ca5d531dc3d8b04f06ea286a0bebedc28e2da4
ratiki <987090402@qq.com>
2019-06-26 19:53:37 +0900

    N皇后问题 ready

--- Algorithm/033/solveNQueens.go
+++ Algorithm/033/solveNQueens.go
@@ -2,5 +2,50 @@ package _33
 
 func solveNQueens(n int) [][]string {
 	result := make([][]string, 0)
+	for i := 0; i < n; i++ {
+		result = append(result, make([]string, n))
+	}
+	helper(result, 0)
+
 	return result
+}
+
+func helper(arr [][]string, row int) {
+	if row == len(arr) {
+		return
+	}
+
+	for i := 0; i < len(arr); i++ {
+		if isValid(arr, row, i) {
+			arr[row][i] = "Q"
+			helper(arr, row+1)
+			arr[row][i] = "."
+		}
+	}
+}
+
+func isValid(arr [][]string, row, col int) bool {
+
+	for i := 0; i < row; i++ {
+		if arr[i][col] == "Q" {
+			return false
+		}
+	}
+
+	for i := row - 1; i >= 0; i-- {
+		for j := col - 1; j >= 0; j-- {
+			if arr[i][j] == "Q" {
+				return false
+			}
+		}
+	}
+
+	for i := row - 1; i >= 0; i-- {
+		for j := col + 1; j < len(arr); j++ {
+			if arr[i][j] == "Q" {
+				return false
+			}
+		}
+	}
+	return true
 }
