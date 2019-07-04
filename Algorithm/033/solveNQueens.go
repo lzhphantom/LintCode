9ff503e92b370daec4e4637884b92b57bca72c3f
ratiki <987090402@qq.com>
2019-07-04 18:04:53 +0900

    n皇后问题解决 理解注释

--- Algorithm/033/solveNQueens.go
+++ Algorithm/033/solveNQueens.go
@@ -129,6 +129,9 @@ func (nq *NQueen) solve() {
 	//fmt.Println("Found", nq.solutions, "solutions.")
 }
 
+// 通过检查所有N个可能的情况，尝试在targetRow上放置一个女王。
+// 如果找到一个有效的位置，该函数会调用自己试图在下一行放置一个女王，
+// 直到所有N个女王被放置在NxN板上。
 func (nq *NQueen) putQueen(position []int, targetRow int) {
 	if targetRow == nq.size {
 		nq.showFullBoard(position)
@@ -143,9 +146,12 @@ func (nq *NQueen) putQueen(position []int, targetRow int) {
 	}
 }
 
+// 检查给定位置是否受到任何先前放置的皇后的攻击（检查列和对角线位置）
 func (nq *NQueen) checkPlace(position []int, row, col int) bool {
 	for i := 0; i < row; i++ {
-		if position[i] == col || position[i]-i == col-row || position[i]+i == col+row {
+		if position[i] == col || /*同一列是否有皇后*/
+			position[i]-i == col-row || /*正斜角上是否有皇后*/
+			position[i]+i == col+row { /*反斜角上是否有皇后*/
 			return false
 		}
 	}
