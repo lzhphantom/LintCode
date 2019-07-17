68876fa5dae5ecccbe3d2e228ac05bd5df991b28
ratiki <987090402@qq.com>
2019-07-17 16:04:53 +0900

    四数之和

---
+++ Algorithm/058/fourSum.go
@@ -0,0 +1,107 @@
+package _58
+
+import "sort"
+
+func fourSum(arr []int, target int) [][]int {
+	if !sort.IntsAreSorted(arr) {
+		sort.Ints(arr)
+	}
+	result := make([][]int, 0)
+	startA := 0
+	startB := startA + 1
+	startC := startB + 1
+	startD := startC + 1
+out:
+	for startA < len(arr) {
+		for startD == len(arr) {
+			startC++
+			startD = startC + 1
+		}
+		if startD > len(arr) {
+			startD = -1
+		}
+		for startC == len(arr) {
+			startB++
+			startC = startB + 1
+			if startD == -1 {
+				startD = startC + 1
+				if startD == len(arr) {
+					startA++
+					startB = startA + 1
+					startC = startB + 1
+					startD = startC + 1
+				}
+			}
+		}
+		if startC > len(arr) {
+			startC = -1
+		}
+		for startB == len(arr) {
+			startA++
+			startB = startA + 1
+			if startC == -1 {
+				startC = startB + 1
+
+			}
+		}
+		if startD == len(arr) {
+			break out
+		}
+		if arr[startA]+arr[startB]+arr[startC]+arr[startD] == target {
+			check([]int{arr[startA], arr[startB], arr[startC], arr[startD]}, &result)
+		}
+		startD++
+	}
+	return result
+}
+
+//for test
+func fourSumOther(arr []int, target int) [][]int {
+	result := make([][]int, 0)
+
+	if !sort.IntsAreSorted(arr) {
+		sort.Ints(arr)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			for k := j + 1; k < len(arr); k++ {
+				for l := k + 1; l < len(arr); l++ {
+					if arr[i]+arr[j]+arr[k]+arr[l] == target {
+						check([]int{arr[i], arr[j], arr[k], arr[l]}, &result)
+					}
+				}
+			}
+		}
+	}
+
+	return result
+}
+
+func check(arr []int, list *[][]int) {
+	var count int
+	for i := 0; i < len(*list); i++ {
+		curArr := append([]int{}, (*list)[i]...)
+
+		if len(curArr) == len(arr) {
+			count = 0
+			start := 0
+			end := len(arr) - 1
+			for start != end {
+				if curArr[start] == arr[start] {
+					count++
+				}
+				start++
+			}
+		} else {
+			continue
+		}
+
+		if count == len(arr) {
+			break
+		}
+	}
+	if count != len(arr) {
+		*list = append(*list, arr)
+	}
+}
---
+++ Algorithm/058/fourSum_test.go
@@ -0,0 +1,47 @@
+package _58
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	tests := []struct {
+		arr    []int
+		target int
+		want   [][]int
+	}{
+		{
+			[]int{2, 7, 11, 15},
+			3,
+			[][]int{},
+		},
+		{
+			[]int{1, 0, -1, 0, -2, 2},
+			0,
+			[][]int{
+				{-2, -1, 1, 2},
+				{-2, 0, 0, 2},
+				{-1, 0, 0, 1},
+			},
+		},
+		{
+			[]int{1, 0, -1, 0, -2, 2, 3},
+			0,
+			[][]int{
+				{-2, -1, 0, 3},
+				{-2, -1, 0, 3},
+				{-2, -1, 1, 2},
+				{-2, 0, 0, 2},
+				{-1, 0, 0, 1},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		result := fourSum(test.arr, test.target)
+		if !reflect.DeepEqual(result, test.want) {
+			t.Errorf("fourSum(%v, %d) => %v,want %v", test.arr, test.target, result, test.want)
+		}
+	}
+}
